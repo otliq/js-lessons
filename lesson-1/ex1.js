feb9f2a4ab98e92eecfdfe6bac845eeb7f324535
otliq <otliq14@gmail.com>
2023-07-29 01:20:33 +0300

    First lesson

---
+++ lesson-1/ex1.js
@@ -0,0 +1,29 @@
+//BismiLLah, This is my first JavaScript code!
+
+/*
+'use strict'; Makes clear to browser that we use ES6 version of JS. Must write it BEFORE JS code. Dont forget ;
+*/
+'use strict';
+
+let obj = document.getElementById('id_div');
+// console.log(obj);
+
+let clr = 'Hello';
+// console.log(clr);
+
+let user = 'Muhammad',
+	age = 20,
+	email = 'my@email.ru';
+
+age = 17;
+email = 'new@email.ru';
+
+age = 'new@email.ru';
+email = 18;
+
+// Best practice to write const variable name with CAP.
+const PI = 3.1415;
+// PI = 2;  const cant be changed. 
+console.log(PI);
+
+// console.log(age);
